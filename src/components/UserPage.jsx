2e8db626900ec4016d8e7e0e81704f9d13b2b801
Nade04 <86410129+Nade0478@users.noreply.github.com>
2025-03-18 13:40:41 +0100

    modif userpage

--- src/components/UserPage.jsx
+++ src/components/UserPage.jsx
@@ -1,23 +1,12 @@
-import React from 'react';
-import {getCurrentUser, logout} from ' ./auth/login';
+import React from "react";
 
-const UserPage = ({onLogout}) => {
-    const user = getCurrentUser();
-
-    const handleLogout = () => {
-        logout();
-        onLogout();
-};
-
-if (!user)  return <p>Tu dois te connecter !.</p>;
-
-return (
+const UserPage = ({ handleLogout }) => {
+  return (
     <div>
-        <h1>Bienvenue {user.username}!</h1>
-        <p>Voici ton adresse email : {user.email}</p>
-        <button onClick={handleLogout}>Se déconnecter</button>
+      <h2>Bienvenue sur votre tableau utilisateur</h2>
+      <button onClick={handleLogout}>Déconnexion</button>
     </div>
-);
+  );
 };
 
 export default UserPage;
