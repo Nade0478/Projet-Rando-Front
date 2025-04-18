d2c7e23ba76d89f77827b2bb208253e3b47c7b37
Nade04 <86410129+Nade0478@users.noreply.github.com>
2025-04-18 02:46:04 +0200

    modif carprofil components + css

--- src/components/profil/ProfileCard.jsx
+++ src/components/profil/ProfileCard.jsx
@@ -9,11 +9,13 @@ const ProfileCard = ({ id_user }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-console.log(id_user);
+  console.log(id_user);
   useEffect(() => {
     const fetchUser = async () => {
       try {
-        const response = await axios.get(`http://127.0.0.1:8000/api/user/${id_user}`);
+        const response = await axios.get(
+          `http://127.0.0.1:8000/api/user/${id_user}`
+        );
         setUser(response.data);
       } catch (err) {
         setError("Erreur lors de la récupération du profil.");
@@ -21,10 +23,10 @@ console.log(id_user);
         setLoading(false);
       }
     };
-  
+
     fetchUser();
   }, [id_user]);
-  
+
   if (loading) {
     return (
       <div className="text-center mt-10">
@@ -35,25 +37,31 @@ console.log(id_user);
   }
 
   if (error || !user) {
-    return <Alert variant="danger">L'utilisateur demandé est introuvable.</Alert>;
+    return (
+      <Alert variant="danger">L'utilisateur demandé est introuvable.</Alert>
+    );
   }
 
   return (
-    <div>
-      <div className="container mt-10 d-flex justify-content-center">
-        <Card className="shadow-lg p-12" style={{ width: "140rem" }}>
-          <Card.Body>
-            <Card.Title className="text-center larger"><strong>{user.name}</strong></Card.Title>
-            <Card.Text>
-              <strong>Email :</strong> {user.email}
-              <br/>
-              <strong>Mot de passe :</strong> {user.password ? user.password : "Non disponible"}
-            </Card.Text>
-          </Card.Body>
-        </Card>
+    <section className="profileCard">
+      <div>
+        <div className="container mt-10 d-flex justify-content-center">
+          <Card className="shadow-lg p-12" style={{ width: "140rem" }}>
+            <Card.Body>
+              <Card.Title className="text-center larger">
+                <strong>{user.name}</strong>
+              </Card.Title>
+              <Card.Text>
+                <strong>Email :</strong> {user.email}
+                <br />
+                <strong>Mot de passe :</strong>{" "}
+                {user.password ? user.password : "Non disponible"}
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </div>
       </div>
-
-    </div>
+    </section>
   );
 };
 
